0442de8d79b9b4fb44e560b8b60560a90bced8b3
Gelo <gelodoescode@users.noreply.github.com>
2023-03-16 02:40:33 +0800

    Create 404 page

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,14 @@
+import style from "./style.module.css"
+import Head from "next/head"
+
+export default function() {
+  return (
+    <>
+      <Head>
+        <title>404 not found</title>
+      </Head>
+      <main className={style.main}>
+      </main>
+    </>
+  )
+}
